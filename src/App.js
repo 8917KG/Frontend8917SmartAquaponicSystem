44dbd415c982526eb4467299cfa6067126022f32
8917KG <8917@ait.nsw.edu.au>
2024-10-31 08:10:33 +1100

    added: bootstrap, react router dom and firebase. Added components and pages folder on src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+
+//import page components
+import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
+import { About } from './pages/About';
+
+const NavData = [
+    {name: "Home", path: "/", public: true},
+    {name: "About", path: "/about", public: true},
+    {name: "Contact", path: "/contact", public: true}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header title = "SAWS" headernav={NavData}/>
+        <Routes>
+            <Route path='/' element =  {<Home/>}  />
+            <Route path='/about' element =  {<About/>}  />
+            <Route path='/contact' element = {<Contact/>} />
+        </Routes>
+        <Footer year = "2024"/>
     </div>
   );
 }
---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyCraun8RYEFicB6w48UixfnMgZ_8MCCoFM",
+  authDomain: "smartaqauponicwateringsystem.firebaseapp.com",
+  projectId: "smartaqauponicwateringsystem",
+  storageBucket: "smartaqauponicwateringsystem.appspot.com",
+  messagingSenderId: "707125814360",
+  appId: "1:707125814360:web:a7dd87e8b346c4b483162d"
+};
